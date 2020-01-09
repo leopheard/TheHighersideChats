d19cb5bddf4e4e1c5fd19778b4ea8f7ba65816dd
leopheard <leopheard.misc@gmail.com>
2020-01-09 14:17:01 -0500

    upload

---
+++ addon.py
@@ -0,0 +1,35 @@
+from xbmcswift2 import Plugin, xbmcgui
+from resources.lib import mainaddon
+
+plugin = Plugin()
+url1 = "https://feeds.podcastmirror.com/higherside-chats"
+@plugin.route('/')
+def main_menu():
+    items = [
+        {
+            'label': plugin.get_string(30001), 
+            'path': plugin.url_for('episodes1'),
+            'thumbnail': "https://is2-ssl.mzstatic.com/image/thumb/Podcasts123/v4/eb/d1/4d/ebd14d73-008e-c679-4220-7165ed0ec1df/mza_6296425942549497376.jpg/600x600bb.jpg"},
+        {
+            'label': plugin.get_string(30000),
+            'path': plugin.url_for('episodes'),
+            'thumbnail': "https://is2-ssl.mzstatic.com/image/thumb/Podcasts123/v4/eb/d1/4d/ebd14d73-008e-c679-4220-7165ed0ec1df/mza_6296425942549497376.jpg/600x600bb.jpg"},
+    ]
+    return items
+
+@plugin.route('/episodes1/')
+def episodes1():
+    soup1 = mainaddon.get_soup1(url1)
+    playable_podcast1 = mainaddon.get_playable_podcast1(soup1)
+    items = mainaddon.compile_playable_podcast1(playable_podcast1)
+    return items
+
+@plugin.route('/episodes/')
+def episodes():
+    soup1 = mainaddon.get_soup1(url1)
+    playable_podcast = mainaddon.get_playable_podcast(soup1)
+    items = mainaddon.compile_playable_podcast(playable_podcast)
+    return items
+
+if __name__ == '__main__':
+    plugin.run()
