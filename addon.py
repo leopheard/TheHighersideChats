e22a9814d852ad46514fcc359b67d686c68d33e5
leopheard <leopheard.misc@gmail.com>
2020-01-09 14:22:35 -0500

    upload

--- addon.py
+++ addon.py
@@ -7,6 +7,11 @@ url1 = "https://feeds.podcastmirror.com/higherside-chats"
 def main_menu():
     items = [
         {
+            'label': plugin.get_string(30002),
+            'path': "http://alpha.podcasttoradio.com/18038",
+            'thumbnail': "",
+            'is_playable': True},
+        {
             'label': plugin.get_string(30001), 
             'path': plugin.url_for('episodes1'),
             'thumbnail': "https://is2-ssl.mzstatic.com/image/thumb/Podcasts123/v4/eb/d1/4d/ebd14d73-008e-c679-4220-7165ed0ec1df/mza_6296425942549497376.jpg/600x600bb.jpg"},
